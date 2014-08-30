e7411de55cac5c5add875d88166c71193bc6d6bb
venky91 <venksundram@gmail.com>
2014-08-30 13:49:09 -0700

    added a getSize method to return size of list

--- DataStructures/LinkedList.py
+++ DataStructures/LinkedList.py
@@ -3,6 +3,7 @@ class LinkedList:
         def __init__(self, value):
             self.value = value;
             self.next = None
+            self.size = 0
 
     def __init__(self):
         self.head = None
@@ -10,24 +11,31 @@ class LinkedList:
     def add(self, value):
         if self.head == None:
             self.head = self.Node(value)
+            self.size = 1
         else:
             temp = self.head
             while temp.next is not None:
                 temp = temp.next
             temp.next = self.Node(value)
+            self.size += 1
 
     def remove(self, value):
+        remove = False
         if self.head is not None:
             if self.head.value == value:
                 self.head = self.head.next;
+                remove = True
 
             else:
                 temp = self.head
                 while temp.next is not None:
                     if temp.next.value == value:
                         temp.next = temp.next.next
+                        remove = True
                         break
                     temp = temp.next
+            if remove is True:
+                self.size -= 1
     
     def printMe(self):
         temp = self.head
@@ -53,6 +61,8 @@ class LinkedList:
 
         return index;
 
+    def getSize(self):
+        return self.size
 
         
             
